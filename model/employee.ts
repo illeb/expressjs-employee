60ea806f4ad03d196158925b800020f5149fcc38
Stefano Belli <>
2021-12-29 14:19:36 +0100

    first scaffolding

---
+++ model/employee.ts
@@ -0,0 +1,11 @@
+export class Quiz {
+  
+  constructor(private id: number | null, 
+    private firstName: string, 
+    private lastName: string, 
+    private birthDate?: Date, 
+    private hireDate?: Date)
+  {
+
+ }
+}
